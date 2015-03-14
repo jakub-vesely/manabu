f6ff7a1faf54f71a83e9fbe09a00e3f265d2f06e
kubus <kubus@kubus.cz>
2015-03-14 19:27:39 +0100

    coommon functionalit separated from rgb specific functionalit

--- PIC/common/common_16F1503.c
+++ PIC/common/common_16F1503.c
@@ -1,4 +1,5 @@
 #include "common_16F1503.h"
+#include "HEFlash.h"
 
 void Common16F1503Init()
 {
@@ -7,6 +8,13 @@ void Common16F1503Init()
 
 	ANSELA = 0x00;      //set analog pins to digital
     ANSELC = 0x00;
+
+	g_mode = HEFLASH_readByte (1, 0);
+
+#ifdef HAVE_OUTPUT
+	SwitchControllerInit();
+#endif
+	I2cSlaveInit();
 }
 
 void Wait(int delay)
@@ -16,4 +24,62 @@ void Wait(int delay)
 	for (i = 0; i < 100 * delay; i++)
 		for (j = 0; j < 50; j++);
 
+}
+
+void SwitchControllerInit()
+{
+	LATA4 = 0;
+	LATA5 = 0;
+	TRISAbits.TRISA4 = 0;
+	TRISAbits.TRISA5 = 0;
+	OUTPUT_SWITCH = 0;
+	INPUT_SWITCH = 1;
+}
+
+void SendMessageToOutput(unsigned char messageType, I2cCommand command, unsigned char const *data, unsigned char count)
+{
+	INPUT_SWITCH = 0;
+	OUTPUT_SWITCH = 1;
+	I2cMasterInit();
+	I2cMasterPut(messageType, command, data, count);
+	I2cSlaveInit();
+	OUTPUT_SWITCH = 0;
+	INPUT_SWITCH = 1;
+}
+
+unsigned char GetMessageFromOutput(unsigned char messageType, I2cCommand command, unsigned char const *data, unsigned char count)
+{
+	unsigned char value;
+	INPUT_SWITCH = 0;
+	OUTPUT_SWITCH = 1;
+	I2cMasterInit();
+	value = I2cMasterGet(messageType, command, data, count);
+	I2cSlaveInit();
+	OUTPUT_SWITCH = 0;
+	INPUT_SWITCH = 1;
+
+	return value;
+}
+
+void ProcessCommand()
+{
+	//unsigned char deleteme;
+	switch(g_commandInstruction)
+	{
+		case COMMAND_CHANGE_MODE:
+			g_commandRecieved = false;
+			g_mode = g_commandValue;
+			HEFLASH_writeBlock(1, (void*)&g_mode, sizeof(g_mode)); //I dont understand why but radd = 0 doesn't work for me
+			g_stateChanged = true;
+			break;
+	}
+}
+
+void ProcessStateChangedCommon()
+{
+	g_stateChanged = false;
+
+	#ifdef HAVE_OUTPUT
+				SendMessageToOutput(I2C_MESSAGE_TYPE_DATA, 0, &g_state, 1);
+	#endif
 }
--- PIC/common/common_16F1503.h
+++ PIC/common/common_16F1503.h
@@ -3,14 +3,22 @@
 
 #include <stdbool.h>
 #include <pic16f1503.h>
+#include <common/i2c.h>
+
+#define INPUT_SWITCH PORTAbits.RA5
+#define OUTPUT_SWITCH PORTAbits.RA4
 
 void Common16F1503Init();
 void Wait(int delay);
+void SwitchControllerInit();
+void SendMessageToOutput(unsigned char messageType, I2cCommand command, unsigned char const *data, unsigned char count);
+unsigned char GetMessageFromOutput(unsigned char messageType, I2cCommand command, unsigned char const *data, unsigned char count);
+void ProcessCommand();
+void ProcessStateChangedCommon();
 
 unsigned char g_mode = 0;
-
 unsigned char g_state = 0;
-bool g_stateChanged = true;
+unsigned char g_stateChanged = true;
 bool g_commandRecieved = false;
 unsigned char g_commandInstruction = 0;
 unsigned char g_commandValue = 0;
