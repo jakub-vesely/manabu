ea42bb6161b33608b8b229fe11621ea8564855e8
kubus <kubus@kubus.cz>
2015-04-04 21:18:51 +0200

    test program for a bootloader

---
+++ PIC/bootTest.X/bootTest.c
@@ -0,0 +1,21 @@
+#include <xc.h>
+#include <pic16f1503.h>
+
+__CONFIG(FOSC_INTOSC & WDTE_OFF & MCLRE_OFF & BOREN_OFF & WRT_OFF & LVP_OFF &CP_OFF);
+
+void main(void)
+{
+	LATC5 = 0;
+	TRISC5 = 0;
+	
+	while(1)
+	{
+		int i = 0;
+		PORTCbits.RC5 = 1;
+		for (;i < 2000; i++);
+
+		PORTCbits.RC5 = 0;
+		for (i = 0;i < 2000; i++);
+
+	}
+}
