8d107757bd6bbd32df014d529d470b2b97610d19
kubus <kubus@kubus.cz>
2015-05-31 21:21:43 +0200

    arthmetic-logic module suport bootlader and works as invertor

--- PIC/arithmetic-logic.X/moduleTypeSpecific.c
+++ PIC/arithmetic-logic.X/moduleTypeSpecific.c
@@ -1,13 +1,10 @@
-#include <common/common.h>
 #include <system_common.h>
+#include <common/common.h>
+#include <CommonConstants.h>
 
 void ProcessStateChangedModuleTypeSpecific()
 {
-	/*if (g_state == 0)
-		g_state = 1;
-	if (g_state == 255)
-		g_state = 254;
-	*/
+	
 }
 
 void ModuleTypeSpecificInit()
@@ -15,12 +12,16 @@ void ModuleTypeSpecificInit()
 	g_state = 0;
 }
 
-void ProcessModuleFunctionalit()
+
+void ProcessModuleFunctionality()
 {
-	/*if (g_state == 254)
-		g_state = 1;
-	else
-		g_state = g_state + 1;
-	Wait(1);
-	g_stateChanged = true;*/
+	if (g_stateChanged)
+	{
+		g_state = 255-g_state; //works as invertor
+	}
+}
+
+unsigned char GetModuleType()
+{
+	return TYPE_ARITHMETIC_LOGIC;
 }
--- PIC/arithmetic-logic.X/moduleTypeSpecific.h
+++ PIC/arithmetic-logic.X/moduleTypeSpecific.h
@@ -2,5 +2,6 @@
 #define _MODULE_TYPE_SPECIFIC_H_
 void ProcessStateChangedModuleTypeSpecific();
 void ModuleTypeSpecificInit();
-void ProcessModuleFunctionalit();
+void ProcessModuleFunctionality();
+unsigned char GetModuleType();
 #endif //_MODULE_TYPE_SPECIFIC_H_
