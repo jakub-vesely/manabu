49d7b28c28a04fed1100b7d6a893401df8722900
kubus <kubus@kubus.cz>
2015-07-30 11:38:14 +0200

    interface specific code for bootloader

--- PIC/bootloader.X/bootloader.c
+++ PIC/bootloader.X/bootloader.c
@@ -7,25 +7,53 @@
 #include <stdbool.h>
 #include <CommonConstants.h>
 
+#if defined (INTERFACE)
 // CONFIG1
-#pragma config FOSC = INTOSC    // Oscillator Selection Bits (INTOSC oscillator: I/O function on CLKIN pin)
-#pragma config WDTE = OFF       // Watchdog Timer Enable (WDT disabled)
-#pragma config PWRTE = OFF      // Power-up Timer Enable (PWRT disabled)
-#pragma config MCLRE = OFF      // MCLR Pin Function Select (MCLR/VPP pin function is digital input)
-#pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
-#pragma config BOREN = ON       // Brown-out Reset Enable (Brown-out Reset enabled)
-#pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
+#	pragma config FOSC = INTOSC    // Oscillator Selection Bits (INTOSC oscillator: I/O function on CLKIN pin)
+#	pragma config WDTE = SWDTEN   // Watchdog Timer Enable (WDT controlled by software)
+#	pragma config PWRTE = OFF      // Power-up Timer Enable (PWRT disabled)
+#	pragma config MCLRE = OFF      // MCLR Pin Function Select (MCLR/VPP pin function is digital input)
+#	pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
+#	pragma config BOREN = ON       // Brown-out Reset Enable (Brown-out Reset enabled)
+#	pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
+#	pragma config IESO = OFF       // Internal/External Switchover Mode (Internal/External Switchover Mode is disabled)
+#	pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable (Fail-Safe Clock Monitor is disabled)
 
 // CONFIG2
-#pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
-#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
-#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
-#pragma config LPBOR = OFF      // Low-Power Brown Out Reset (Low-Power BOR is disabled)
-#pragma config LVP = OFF        // Low-Voltage Programming Enable (High-voltage on MCLR/VPP must be used for programming)
+#	pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
+#	pragma config CPUDIV = NOCLKDIV// CPU System Clock Selection Bit (NO CPU system divide)
+#	pragma config USBLSCLK = 48MHz // USB Low SPeed Clock Selection bit (System clock expects 48 MHz, FS/LS USB CLKENs divide-by is set to 8.)
+#	pragma config PLLMULT = 3x     // PLL Multipler Selection Bit (3x Output Frequency Selected)
+#	pragma config PLLEN = ENABLED  // PLL Enable Bit (3x or 4x PLL Enabled)
+#	pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
+#	pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
+#	pragma config LPBOR = OFF      // Low-Power Brown Out Reset (Low-Power BOR is disabled)
+#	pragma config LVP = OFF        // Low-Voltage Programming Enable (High-voltage on MCLR/VPP must be used for programming)
+#else
+// CONFIG1
+#	pragma config FOSC = INTOSC    // Oscillator Selection Bits (INTOSC oscillator: I/O function on CLKIN pin)
+#	pragma config WDTE = OFF       // Watchdog Timer Enable (WDT disabled)
+#	pragma config PWRTE = OFF      // Power-up Timer Enable (PWRT disabled)
+#	pragma config MCLRE = OFF      // MCLR Pin Function Select (MCLR/VPP pin function is digital input)
+#	pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
+#	pragma config BOREN = ON       // Brown-out Reset Enable (Brown-out Reset enabled)
+#	pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
+
+// CONFIG2
+#	pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
+#	pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
+#	pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
+#	pragma config LPBOR = OFF      // Low-Power Brown Out Reset (Low-Power BOR is disabled)
+#	pragma config LVP = OFF        // Low-Voltage Programming Enable (High-voltage on MCLR/VPP must be used for programming)
+#endif
 
 #define VERSION 1
 #define MAIN_PROOGRAM_START 0x100
-#define MAIN_PROOGRAM_MAX 0x7FF
+#if defined (INTERFACE)
+#	define MAIN_PROOGRAM_MAX 0x1FFF
+#else
+#	define MAIN_PROOGRAM_MAX 0x7FF
+#endif
 #define RUN_PROGRAM_FLAG_POSITION HEFLASH_START
 
 unsigned g_flashAddr;
@@ -85,7 +113,7 @@ void ReadI2C()
 				//I would have to have booltloader sise 0x200 so I will protect
 				//it this way. I probabl shoud increase checksum but I will have
 				//to consume a more memory space. It will not happend by
-				//a correct way and checksum will not maptch anwway
+				//a correct way and checksum will not maptch anyway
 				if (g_flashAddr < MAIN_PROOGRAM_START || g_flashAddr > MAIN_PROOGRAM_MAX)
 					g_flashAddr = MAIN_PROOGRAM_START;
 
