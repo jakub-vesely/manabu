773009ee6ada0a8afbbf8f89e90c70e6f7819142
jakub-vesely <jakub_vesely@seznam.cz>
2017-01-12 21:08:41 +0100

    Generator

--- PIC/generator/moduleTypeSpecific.c
+++ PIC/generator/moduleTypeSpecific.c
@@ -2,33 +2,77 @@
 #include <common/common.h>
 #include <CommonConstants.h>
 
+#define MULTIPLIER 10
+bool g_increase = true;
+typedef enum
+{
+	MODE_SAW = 0,
+    MODE_TRIANGLE = 1,	
+    MODE_COUNT = 2
+} MODE;
+
 void ProcessCommandModuleTypeSpecific()
 {
+    
 }
 void ProcessStateChangedModuleTypeSpecific()
 {
-	
+    switch (g_persistant.mode)
+    {
+        case MODE_SAW:
+            g_outState += g_inState / MULTIPLIER;
+            if (g_outState > STATE_MAX)
+                g_outState = 0;
+        break;
+        case MODE_TRIANGLE:
+            if (g_increase)
+            {
+                if (g_outState > STATE_MAX - g_inState / MULTIPLIER)
+                {
+                    g_outState = STATE_MAX;
+                    g_increase = false;
+                }
+                else
+                    g_outState += g_inState / MULTIPLIER;
+                
+                    
+            }
+            else
+            {
+                if (g_outState < g_inState / MULTIPLIER)
+                {
+                    g_outState = 0;
+                    g_increase = true;
+                }
+                else
+                    g_outState -= g_inState / MULTIPLIER;
+                
+                
+            }
+        break;
+    }
+    
+    //}
+	//g_outState = g_inState / 2;
+    //g_outState = STATE_MAX - g_inState; //works as invertor
 }
 
 void ModuleTypeSpecificInit()
 {
-	g_inState = 1;
+    g_outState = 0;
 }
-
-
 
 
 void ProcessModuleFunctionality()
 {
-	g_outState = g_inState;
 }
 
 unsigned char GetModuleType()
 {
-	return TYPE_POTENTIOMETER;
+	return TYPE_GENERATOR;
 }
 
 unsigned char GetModeCount()
 {
-    return 1;
+    return MODE_COUNT;
 }
